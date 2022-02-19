8b293ea0641296c5b7186fa6f9b0669282cce6e4
Kierro DeCampanille <pentawick@gmail.com>
2022-02-19 21:56:20 +0800

    async edit

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,5 +1,5 @@
 const {readFile, writeFile} = require('fs');
-
+console.log('start');
 readFile('./content/first.txt','utf-8',(err,result)=>{
     if(err){
         console.log(err);
@@ -22,12 +22,9 @@ readFile('./content/first.txt','utf-8',(err,result)=>{
                     console.log(err)
                     return;
                 }
-                console.log(result);        
+                console.log('done with this tast');        
             }
         )
-
-
-
-
     })
 })
+console.log('starting next task');
